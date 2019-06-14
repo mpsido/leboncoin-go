63b95666d1271c5063e820e9ac564a1486a61843
mpsido <1979371837@qq.com>
2019-06-15 01:03:10 +0800

    Create a gorila mux server performing the fizzbuzz query

--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func do_fizzbuzz(int1 uint, int2 uint, limit uint, str1 string, str2 string) {
-	
+	"github.com/gorilla/mux"
+)
+
+func do_fizzbuzz(int1 uint, int2 uint, limit uint, str1 string, str2 string) string {
+
 	fizzbuzz := fmt.Sprintf(" %s%s,", str1, str2)
 	fizz := fmt.Sprintf(" %s,", str1)
 	buzz := fmt.Sprintf(" %s,", str2)
 	fizzbuzzString := ""
 	for i := uint(1); i < limit; i++ {
-		if i % (int1 * int2) == 0 {
+		if i%(int1*int2) == 0 {
 			fizzbuzzString += fizzbuzz
-		} else if i % int1 == 0 {
+		} else if i%int1 == 0 {
 			fizzbuzzString += fizz
-		} else if i % int2 == 0 {
+		} else if i%int2 == 0 {
 			fizzbuzzString += buzz
 		} else {
 			fizzbuzzString += fmt.Sprintf(" %d,", i)
 		}
 	}
-	fmt.Println(fizzbuzzString)
+	return fizzbuzzString
+}
+
+func FizzBuzz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", do_fizzbuzz(3, 5, 100, "fizz", "buzz"))
 }
 
 func main() {
-	do_fizzbuzz(3, 5, 100, "fizz", "buzz")
+	s := do_fizzbuzz(3, 5, 100, "fizz", "buzz")
+	fmt.Println(s)
+	r := mux.NewRouter()
+	r.HandleFunc("/", FizzBuzz)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
