5ab734ffbf515e13eab72642801ff3f9c479feca
mpsido <1979371837@qq.com>
2019-06-15 18:24:03 +0800

    Implemeting first version Stat API call

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var statMap = make(map[string]int)
+
 func do_fizzbuzz(int1 uint, int2 uint, limit uint, str1 string, str2 string) string {
 
 	fizzbuzz := fmt.Sprintf(" %s%s,", str1, str2)
@@ -27,6 +29,20 @@ func do_fizzbuzz(int1 uint, int2 uint, limit uint, str1 string, str2 string) str
 		}
 	}
 	return fizzbuzzString
+}
+
+func Stats(w http.ResponseWriter, r *http.Request) {
+	max := 0
+	index := ""
+	for k, v := range statMap {
+		if v > max {
+			max = v
+			index = k
+		}
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	log.Printf("Most used query is %s, used %d times\n", index, max)
+	fmt.Fprintf(w, "Most used query is %s, used %d times", index, max)
 }
 
 func FizzBuzz(w http.ResponseWriter, r *http.Request) {
@@ -72,7 +88,13 @@ func FizzBuzz(w http.ResponseWriter, r *http.Request) {
 	if int1, err := strconv.ParseUint(sInt1[0], 10, 32); err == nil {
 		if int2, err := strconv.ParseUint(sInt2[0], 10, 32); err == nil {
 			if limit, err := strconv.ParseUint(sLimit[0], 10, 32); err == nil {
+				queryString := fmt.Sprintf("%d-%d-%d-%s-%s", int1, int2, limit, str1[0], str2[0])
 				fmt.Fprintf(w, "%s", do_fizzbuzz(uint(int1), uint(int2), uint(limit), str1[0], str2[0]))
+				if _, ok := statMap[queryString]; ok {
+					statMap[queryString]++
+				} else {
+					statMap[queryString] = 1
+				}
 			}
 		}
 	}
@@ -81,6 +103,7 @@ func FizzBuzz(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", FizzBuzz)
+	r.HandleFunc("/stats", Stats)
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
